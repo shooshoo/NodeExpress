9ea2d7fa16a98c6acb2b3503b61233a463ccd6e8
shilan_hk <shilan_hk@yahoo.com>
2020-09-15 17:22:42 +0430

    Express route

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const dishRouter = require('./routes/dishRouter');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -9,59 +10,13 @@ const port = 3000;
 const app = express();
 
 // using middlewares
+
 app.use(morgan('dev'));
 app.use(bodyParser.json()); // will automatically parse body of the req to an object name body
+
 ///////////////////////
 
-
-///////////////  Dishes /////////////////////
-
-app.all('/dishes', (req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next(); // goes to the next command related to this url '/dishes'
-});
-
-app.get('/dishes', (req, res, next) => {
-    res.end('Will send all the dishes to you!');
-});
-
-app.post('/dishes', (req, res, next) => {
-    res.end('Will add the dish: ' + req.body.name + ' with details: ' + req.body.description);
-});
-
-app.put('/dishes', (req, res, next) => {
-    res.statusCode = 403;
-    res.end('PUT operation not supported on /dishes');
-});
-
-app.delete('/dishes', (req, res, next) => {
-    res.end('Deleting all dishes');
-});
-
-///////////////////////////////////////////////
-
-/////////////////  Dish ID ///////////////////////////////
-
-app.get('/dishes/:dishId', (req, res, next) => {
-    res.end('Will send details of the dish: ' + req.params.dishId + ' to you!');
-});
-
-app.post('/dishes/:dishId', (req, res, next) => {
-    res.statusCode = 403;
-    res.end('POST operation not supported on /dishes/' + req.params.dishId);
-});
-
-app.put('/dishes/:dishId', (req, res, next) => {
-    res.write('Updating the dish: ' + req.params.dishId + '\n');
-    res.end('Will update the dish: ' + req.body.name + ' with details: ' + req.body.description);
-});
-
-app.delete('/dishes/:dishId', (req, res, next) => {
-    res.end('Deleting dish: ' + req.params.dishId);
-});
-
-//////////////////////////////////////////////////////////
+app.use('/dishes', dishRouter);
 
 app.use(express.static(__dirname + '/public')); // give it where it should reach the statis pages
 
